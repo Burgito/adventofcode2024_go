a84b14fd968abefb02c687177a875f47dccb8a0b
Mickaël BERGER <mberger@iocean.fr>
2024-12-05 17:11:56 +0100

    resolved j3 part1

--- j3/main.go
+++ j3/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"adventofcode/inputreaders"
+	"adventofcode/ints"
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 func part1() {
 	lines := inputreaders.ReadLinesAsStrings("./input.txt")
-	fmt.Println(lines)
+	re := regexp.MustCompile(`mul\([0-9]{1,3},[0-9]{1,3}\)`)
+	total := 0
+	// multipliers := []int{}
+	for _, line := range lines {
+		//fmt.Printf("%q\n", re.FindAll([]byte(line), -1))
+		for _, mul := range re.FindAllString(line, -1) {
+			values := strings.ReplaceAll(strings.ReplaceAll(mul, "mul(", ""), ")", "")
+			total += (ints.ParseIntFromString(strings.Split(values, ",")[0]) * ints.ParseIntFromString(strings.Split(values, ",")[1]))
+		}
+	}
+
+	fmt.Println("Total of mul : ", total)
 }
 
 func part2() {
